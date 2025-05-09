c8e75c66c5d6139eb28ec21fe3b975d93332f92f
Gia Bao <capricos2212@gmail.com>
2025-05-09 16:13:13 +0700

    [create] popup setting game

--- Assets/MyGame/Scripts/UI/Screen/ScreenMenuGame.cs
+++ Assets/MyGame/Scripts/UI/Screen/ScreenMenuGame.cs
@@ -3,11 +3,14 @@ using UnityEngine.UI;
 
 public class ScreenMenuGame : BaseScreen
 {
+    [Header("Button List")]
     [SerializeField] private Button m_PlayBtn;
+    [SerializeField] private Button m_MenuBtn;
 
     private void Start()
     {
         m_PlayBtn.onClick.AddListener(OnClickPlayButton);
+        m_MenuBtn.onClick.AddListener(OnClickMenuBtn);
     }
 
     private void OnClickPlayButton()
@@ -18,4 +21,11 @@ public class ScreenMenuGame : BaseScreen
             this.Hide();
         }    
     }    
+    private void OnClickMenuBtn()
+    {
+       if(UIManager.HasInstance)
+        {
+            UIManager.Instance.ShowPopup<PopupSettingGame>();
+        }
+    }
 }
